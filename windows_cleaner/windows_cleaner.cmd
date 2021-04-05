fef7c2ce11a10e0644d40f6146a5fa30eeeac7f4
Andrej Šišila <kyberdrb@gmail.com>
2021-04-05 21:25:06 +0200

    Alternatives for priviledge elevation

--- windows_cleaner/windows_cleaner.cmd
+++ windows_cleaner/windows_cleaner.cmd
@@ -1,2 +1,15 @@
-cleanmgr /VERYLOWDISK
-runas /noprofile /user:Administrator "cmd -c cleanmgr /VERYLOWDISK"
+REM Try 'nircmd', 'psexec' or 'gsudo' to elevate priviledges 
+REM  for cleaning up the system files, instead of 'runas' or 
+REM  'powershell' - for compatibility with Windows 
+REM  systems without PowerShell
+REM  - https://www.nirsoft.net/utils/nircmd.html
+REM  - https://docs.microsoft.com/en-us/sysinternals/downloads/psexec
+REM  - https://github.com/gerardog/gsudo
+
+cleanmgr /SAGESET:0
+REM runas /noprofile /user:ClickMe "cleanmgr /SAGESET:0"
+powershell -Command "Start-Process cleanmgr /SAGESET:0 -Verb RunAs"
+
+cleanmgr /SAGERUN:0
+REM runas /noprofile /user:ClickMe "cleanmgr /SAGERUN:0"
+powershell -Command "Start-Process cleanmgr /SAGERUN:0 -Verb RunAs"
